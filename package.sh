a7ce50a0b5b5cba0643383efd812bc2654ebe829
tinmar <martin.hermant@gmail.com>
2021-11-23 16:04:27 +0100

    WIP

--- package.sh
+++ package.sh
@@ -1,14 +1,17 @@
 set -x
 EPATH=/home/tinmar/Dev/raspestrio/androidjs/serverdist
 
-rm -r $EPATH
+rm -rf $EPATH
 mkdir $EPATH
 # rm -r node_modules
 npm run buildForAndroid
 cp -r ../view-dist $EPATH
 
 cp package.json $EPATH
+cp package-lock.json $EPATH
+cp -r node_modules $EPATH
 cd $EPATH
+npm prune --production
 # pnpm i --prod
 npm install --only=prod
 rm package.json
