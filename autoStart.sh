61abe1e311c32b2571c7cdd2d00d5d8a3a640ca9
rpi <martin.hermant@gmail.com>
2022-11-15 10:17:41 +0100

    add nvm version to start scripts

--- autoStart.sh
+++ autoStart.sh
@@ -1,1 +1,2 @@
-npm run run
+#/home/pi/.nvm/versions/node/v16.15.1/bin/npm run run -- -c
+/home/pi/.nvm/versions/node/v16.15.1/bin/node --experimental-modules --es-module-specifier-resolution=node  out/index.js -c
