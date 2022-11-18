7ef795c206ace72d5f0cee84e935b7af1b56a7b0
tinmar <martin.hermant@gmail.com>
2022-11-18 17:08:09 +0100

    only start server

--- autoStartServer.sh
+++ autoStartServer.sh
@@ -1,3 +1,3 @@
 #cd server
 # npm run run -- --srv
-/home/pi/.nvm/versions/node/v16.15.1/bin/node --experimental-modules --es-module-specifier-resolution=node  out/index.js --srv -c
+/home/pi/.nvm/versions/node/v16.15.1/bin/node --experimental-modules --es-module-specifier-resolution=node out/index.js --srv
