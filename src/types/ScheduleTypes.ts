72c3530fed5d8f007f4cf1ec8afebe111d6fcc29
tinmar <martin.hermant@gmail.com>
2022-11-24 09:55:41 +0100

    add support for getting next agenda changers

--- src/types/ScheduleTypes.ts
+++ src/types/ScheduleTypes.ts
@@ -2,6 +2,23 @@
 // Agendas
 import * as dbg from '../dbg'
 
+/// ////////////////
+// conversion helpers
+
+function twoDig(n: number) {
+  return ('0' + n).slice(-2)
+}
+export function dateFromDayAndHourString(d: string, h: string): Date {
+  const res = new Date(dateDayFromString(d))
+  res.setHours(0)
+  console.log('hToMin', hourStringToMinutes(h), h)
+  res.setMinutes(hourStringToMinutes(h) || 0)
+  return res
+}
+export function dateToHourString(d: Date): string {
+  return hourMinutesToString(d.getHours() * 60 + d.getMinutes())
+}
+
 export function hourStringToMinutes(h: string): number | undefined {
   const spl = h.split(':')
   if (spl.length === 2) {
@@ -11,7 +28,22 @@ export function hourStringToMinutes(h: string): number | undefined {
 }
 
 export function hourMinutesToString(h: number): string {
-  return Math.ceil(h / 60) + ':' + Math.ceil(h % 60)
+  return twoDig(Math.floor(h / 60)) + ':' + twoDig(Math.floor(h % 60))
+}
+
+export function dateDayToString(d: Date): string {
+  return '' + twoDig(d.getDate()) + '/' + twoDig(d.getMonth() + 1) + '/' + d.getFullYear()
+}
+
+export function dateDayFromString(d: string): Date {
+  const dspl = d.split('/')
+  const spl = dspl.map(e => { return parseInt(e) })
+  if (spl.length === 3) {
+    return new Date(spl[2], spl[1] - 1, spl[0], 12)
+  } else {
+    dbg.error("can't convert day", d, spl)
+    return new Date()
+  }
 }
 
 export interface HourRange {
@@ -79,20 +111,6 @@ export interface Agenda {
   agendaExceptionList: AgendaException[]
 }
 
-export function dateDayToString(d: Date): string {
-  return '' + d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear()
-}
-
-export function dateDayFromString(d: string): Date {
-  const dspl = d.split('/')
-  const spl = dspl.map(e => { return parseInt(e) })
-  if (spl.length === 3) {
-    return new Date(spl[2], spl[1] - 1, spl[0], 12)
-  } else {
-    dbg.error("can't convert", spl)
-    return new Date()
-  }
-}
 export function createAgendaException(name: string): AgendaException {
   return {
     name,
@@ -126,6 +144,9 @@ export function isActiveForDayType(d: Date, day: DayType): boolean {
     if (end === 0) {
       end = 24 * 60 // end is  midnight assume is next day
     }
+    if (end < st) {
+      end += 24 * 60 // end is before, assume next day
+    }
     if (end > st) {
       // dbg.log('>>>>>>', d.getHours(), curMinutes, st, end)
       return curMinutes >= st && curMinutes < end
@@ -153,4 +174,132 @@ export function isAgendaActiveForDate(d: Date, ag: Agenda): boolean {
   const dn = dayNames[dow]
   const dt = ag.defaultWeek.exceptions.find(e => e.dayName === dn) || ag.defaultWeek.defaultDay as DayType
   return isActiveForDayType(d, dt)
+}
+
+interface HourRangeChangeType {
+  hasValidChange: boolean
+  willBeInsideHourRage: boolean
+  hourString: string
+  diffMin: number
+}
+
+function getNextChangeDateFromHourList(hourString: string, r: HourRange[]): HourRangeChangeType {
+  const res = {
+    hasValidChange: false,
+    willBeInsideHourRage: false,
+    hourString: '',
+    diffMin: 999999999
+  }
+
+  const fromHourMinutes = hourStringToMinutes(hourString)
+  if (fromHourMinutes === undefined) return res
+
+  r.map(h => {
+    const st = hourStringToMinutes(h.start)
+    let end = hourStringToMinutes(h.end)
+    if ((st === undefined) || (end === undefined)) return
+    if (end === 0) {
+      end += 24 * 60 // midnight assume next day
+    }
+    if (end < st) {
+      end += 24 * 60 // if end is before, assume next day
+    }
+    let diffMin = st - fromHourMinutes
+    if (diffMin > 0 && diffMin < res.diffMin) {
+      res.hasValidChange = true
+      res.willBeInsideHourRage = true
+      res.diffMin = diffMin
+      res.hourString = h.start
+    }
+    diffMin = end - fromHourMinutes
+    if (diffMin > 0 && diffMin < res.diffMin) {
+      res.diffMin = diffMin
+      res.hasValidChange = true
+      res.willBeInsideHourRage = false
+      res.hourString = h.end
+    }
+  })
+
+  return res
+}
+
+interface AgendaChangeType {
+  isValid: boolean
+  willBeOn: boolean
+  dateString: string
+  hourString: string
+}
+
+function getNextChangeHourForDay(fromDay: Date, ag: Agenda): AgendaChangeType {
+  const res = {
+    isValid: false,
+    willBeOn: false,
+    dateString: '',
+    hourString: ''
+  }
+
+  const dateString = dateDayToString(fromDay)
+  const fromHourString = dateToHourString(fromDay)
+  const fromMinutes = hourStringToMinutes(fromHourString)
+  // console.log('checking next change for', fromDay.getHours(), dateString, fromHourString, fromMinutes)
+  if (fromMinutes === undefined) { console.error('invalid fromHourString', fromHourString); return res }
+
+  // check exceptions
+  const actDay = new Date(fromDay.getFullYear(), fromDay.getMonth(), fromDay.getDate(), 12)
+  const ex = (ag.agendaExceptionList || []).find(e => {
+    const startD = dateDayFromString(e.dates.start)
+    const endD = dateDayFromString(e.dates.end)
+    return (actDay >= startD && actDay <= endD)
+  })
+  if (ex) {
+    dbg.log('found exception Period', ex.dayValue)
+
+    const nextCh = getNextChangeDateFromHourList(fromHourString, ex.dayValue.hourRangeList)
+    if (nextCh.hasValidChange) {
+      res.isValid = true
+      res.willBeOn = nextCh.willBeInsideHourRage
+      res.dateString = dateString
+      res.hourString = nextCh.hourString
+    }
+    return res
+  }
+
+  // check normal week
+  const dow = (actDay.getDay() + 6) % 7
+  const dn = dayNames[dow]
+  const dt = ag.defaultWeek.exceptions.find(e => e.dayName === dn) || ag.defaultWeek.defaultDay as DayType
+  const nextCh = getNextChangeDateFromHourList(fromHourString, dt.hourRangeList)
+  if (nextCh.hasValidChange) {
+    res.isValid = true
+    res.willBeOn = nextCh.willBeInsideHourRage
+    res.dateString = dateString
+    res.hourString = nextCh.hourString
+  }
+  return res
+}
+
+export function getNextChangeDateFrom(fromD: Date, ag: Agenda): AgendaChangeType {
+  let res = {
+    isValid: false,
+    willBeOn: false,
+    dateString: '',
+    hourString: ''
+  }
+
+  fromD.getHours()
+  const maxDayToCheck = new Date(fromD)
+  maxDayToCheck.setDate(fromD.getDate() + 100)
+  for (let checkedDay = new Date(fromD); checkedDay.getTime() <= maxDayToCheck.getTime(); checkedDay.setDate(checkedDay.getDate() + 1)) {
+    const d = new Date(checkedDay)
+    res = getNextChangeHourForDay(d, ag)
+    if (res.isValid) {
+      return res
+    }
+
+    checkedDay = new Date(checkedDay.getFullYear(), checkedDay.getMonth(), checkedDay.getDate(), 0)// after one iteration day should be checked from midnight
+  }
+
+  console.warn('no agenda change  found')
+
+  return res
 }
