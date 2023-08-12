4f68c7514bfa9877ad0530b94ed346f742adc340
tinmar <martin.hermant@gmail.com>
2023-08-12 17:53:38 +0200

    support new pis

--- src/platformUtil.ts
+++ src/platformUtil.ts
@@ -1,4 +1,4 @@
 import { execSync, execFileSync } from "child_process"
 const proc = execSync("uname -a").toString()
-export const isPi = proc.includes("armv")
+export const isPi = proc.includes("armv") || proc.includes("aarch64")
 export const isOSX = proc.includes("Darwin")
