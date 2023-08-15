83ead4f61c7be800206e28c5d4067308d02a21f4
tinmar <martin.hermant@gmail.com>
2023-08-15 10:48:52 +0200

    use pnpm

--- autoStart.sh
+++ autoStart.sh
@@ -1,2 +1,2 @@
-#/home/pi/.nvm/versions/node/v16.15.1/bin/npm run run -- -c
-/home/pi/.nvm/versions/node/v16.15.1/bin/node --experimental-modules --es-module-specifier-resolution=node  out/index.js -c
+#/home/pi/.local/share/pnpm/npm run run -- -c
+/home/pi/.local/share/pnpm/node --experimental-modules --es-module-specifier-resolution=node out/index.js -c
--- autoStartServer.sh
+++ autoStartServer.sh
@@ -1,3 +1,3 @@
 #cd server
 # npm run run -- --srv
-/home/pi/.nvm/versions/node/v16.15.1/bin/node --experimental-modules --es-module-specifier-resolution=node out/index.js --srv
+/home/pi/.local/share/pnpm/node --experimental-modules --es-module-specifier-resolution=node out/index.js --srv
