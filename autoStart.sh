c45a1332f7536d8be1e4030b87116a4d48f83222
tinmar <martin.hermant@gmail.com>
2023-08-15 15:40:00 +0200

    force npm if available

--- autoStart.sh
+++ autoStart.sh
@@ -1,2 +1,8 @@
+RCF="/home/pi/.bash_profile"
+if [[ -f $RCF ]]; then
+    echo "init pi"
+    source $RCF
+fi
+
 #/home/pi/.local/share/pnpm/npm run run -- -c
-sudo /home/pi/.local/share/pnpm/node --experimental-modules --es-module-specifier-resolution=node out/index.js -c
+sudo node --experimental-modules --es-module-specifier-resolution=node out/index.js -c
--- autoStartServer.sh
+++ autoStartServer.sh
@@ -1,3 +1,8 @@
+RCF="/home/pi/.bash_profile"
+if [[ -f $RCF ]]; then
+    echo "init pi"
+    source $RCF
+fi
 #cd server
 # npm run run -- --srv
-/home/pi/.local/share/pnpm/node --experimental-modules --es-module-specifier-resolution=node out/index.js --srv
+node --experimental-modules --es-module-specifier-resolution=node out/index.js --srv
