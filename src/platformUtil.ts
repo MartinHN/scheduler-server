e0923d2061a63d9eda9f8d49573a160ccd69dcfe
tinmar <martin.hermant@gmail.com>
2023-08-17 09:47:04 +0200

    fake XCOMP ENv??

--- src/platformUtil.ts
+++ src/platformUtil.ts
@@ -1,4 +1,4 @@
 import { execSync, execFileSync } from "child_process"
 const proc = execSync("uname -a").toString()
-export const isPi = proc.includes("armv") || proc.includes("aarch64")
+export const isPi = process.env["PI_XCOMP"] || proc.includes("armv") || proc.includes("aarch64")
 export const isOSX = proc.includes("Darwin")
