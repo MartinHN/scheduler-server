7d9cf1aa78c6e3078947e1ec51eb7c918112edaf
tinmar <martin.hermant@gmail.com>
2023-08-17 16:20:15 +0200

    clean lora and get rid of python dep

--- src/modules/LoraModule.ts
+++ src/modules/LoraModule.ts
@@ -5,51 +5,21 @@ import * as uConf from '../userConf'
 import * as sys from '../sysUtils'
 import { isPi } from '../platformUtil';
 import * as appPaths from '../filePaths'
-import { execSync } from 'child_process';
+import { exec, execSync } from 'child_process';
 import { LoraState, DefaultLoraState, validateLoraState } from '../types/LoraState';
+import unix from "unix-dgram"
+import fs from 'fs'
+import * as lora from './LoraModuleHelpers'
 
-
-
-function execOnPiOnly(cmd) {
-    console.log('cmd will run : ' + cmd)
-    if (isPi)
-        return execSync(cmd).toString();
-    else
-        execSync('sleep 1');
-}
-
-function sysctlCmd(opts: string) {
-    return execOnPiOnly(`sudo systemctl ${opts} lora.service`)
-}
-
-const defaultHexConf = "C200001B2844"
-
-
-function buildHexConfFromState(o: LoraState) {
-    //isPi ? execOnPiOnly("e32 -s | grep Settings Raw Value:") :
-    //  "0xc000001b2844"// 
-    if (!validateLoraState(o))
-        throw new Error("invalid state")
-    let b = hexStrToBuf(defaultHexConf);
-    if (b.byteLength != 6) {
-        throw new Error("invalid")
-    }
-    const SPED = 3
-    const CHAN = 4
-    const OPTION = 5
-    b = setDecBits(b, SPED, 0, 3, o.speed)
-    b = setDecBits(b, CHAN, 0, 6, o.channel)
-    b = setBit(b, OPTION, 2, o.fec);
-
-    let res = bufToHexStr(b)
-    return res
-}
 
 export default class LoraModule {
     confWatcher: ConfFileWatcher;
     public state: LoraState;
     public confFile = appPaths.getConf().baseDir + "/lora.json"
     private confTimeout
+
+    private csock;
+
     constructor(httpApp: Express.Application) {
         this.initHttpEndpoints(httpApp)
         this.confWatcher = new ConfFileWatcher(this.confFile, obj => { this.parseConf(obj) }, new DefaultLoraState());
@@ -59,43 +29,68 @@ export default class LoraModule {
         if (!validateLoraState(o, true))
             console.error("received incomplete lora state", o)
         this.state = o;
-        if (this.isServiceEnabled() != !!o.isActive)
-            this.activateService(!!o.isActive)
-        if (this.confTimeout)
-            clearTimeout(this.confTimeout)
-        this.confTimeout = setTimeout(() => this.setHexConf(buildHexConfFromState(this.state)), 1000)
+        this.setServiceStartsOnBoot(!!o.isActive);
+        if (!!o.isActive) {
+            this.setHexConf(lora.buildHexConfFromState(this.state))
+        }
+        else {
+            this.setServiceRunning(false)
+        }
+
     }
 
 
     setHexConf(hex) {
         console.log("should set e32 bin conf to " + hex)
-        if (hex.length != defaultHexConf.length)
+        if (hex.length != lora.defaultHexConf.length)
             throw new Error("invalid lora config")
 
         this.setServiceRunning(false);
-        setTimeout(() => {
-            if (this.isServiceEnabled())
-                throw Error("e32 sevice shold be disabled!!!")
-            execOnPiOnly(`e32 -w ${hex}  --in-file /dev/null`)
-            if (this.state.isActive)
-                this.setServiceRunning(true);
-        }, 100)
+        lora.execOnPiOnly(`e32 -w ${hex}  --in-file /dev/null`)
+        if (this.state.isActive)
+            this.setServiceRunning(true);
+
     }
 
-    activateService(b: boolean) {
+    sendBufToLora(buf: Buffer) {
+        if (!this.csock) {
+            throw Error("[loraSock] not created")
+        }
+        this.csock.sendBuf(buf)
+    }
+
+    closeSock() {
+        if (this.csock) {
+            this.csock.close()
+            this.csock = undefined
+        }
+    }
+
+    setServiceStartsOnBoot(b: boolean) {
         uConf.setRW(true)
-        sysctlCmd((b ? 'enable' : 'disable '))
+        lora.sysctlCmd((b ? 'enable' : 'disable '))
         uConf.setRW(false)
-        this.setServiceRunning(b)
     }
 
     setServiceRunning(b: boolean) {
-        sysctlCmd(b ? 'start' : 'stop  e32.service ')
+        this.closeSock()
+        lora.sysctlCmd(b ? 'start' : 'stop')
+        if (b) {
+            console.log('[loraSock] rebind sock ');
+            this.csock = lora.createSock(this.processLoraMsg.bind(this))
+        }
     }
 
-    isServiceEnabled() {
+    processLoraMsg(buf: Buffer) {
+        console.log("new lora msg", buf.toString());
+        // // send ack
+        console.log("[loraSock] sendAck")
+        this.sendBufToLora(buf)
+    }
+
+    isServiceRunning() {
         if (!isPi) return this.state.isActive
-        return execOnPiOnly(`systemctl status lora.service --no-pager; echo $?`).toString() === "0";
+        return lora.execOnPiOnly(`systemctl status e32.service --no-pager; echo $?`).toString() === "0";
     }
 
 
@@ -121,7 +116,7 @@ export default class LoraModule {
 
         app.get('/lora/e32config', (req, res) => {
             // res.setHeader('Content-Type', 'application/json');
-            res.send(execOnPiOnly(`e32 -sv`))
+            res.send(lora.execOnPiOnly(`e32 -sv`))
         })
 
     }
@@ -129,65 +124,3 @@ export default class LoraModule {
 }
 
 
-/////////////
-// bit helpers
-
-
-function hexStrToBuf(s) {
-    return Buffer.from(s, 'hex')
-
-}
-function bufToHexStr(b: Buffer) {
-    let res = ""
-    for (let i = 0; i < b.length; i++) {
-        let c = b[i].toString(16).toUpperCase()
-        if (c.length < 2) c = "0" + c
-        res += c
-    }
-
-    return res
-}
-function dec2bin(dec, alignMod = 4) {
-    let r = (dec >>> 0).toString(2);
-    while (alignMod > 0 && ((r.length % alignMod) != 0))
-        r = "0" + r
-    return r;
-}
-
-function bin2dec(bin) {
-    return parseInt(bin, 2);
-}
-
-function setBit(buf, byte, bit, value) {
-    const c = !!value ? "1" : "0"
-    const v = buf[byte];
-    // console.log(v)
-    let o = dec2bin(buf[byte]);
-    // console.log(o)
-    const lsb = 7 - bit
-    o = o.substring(0, lsb) + c + o.substring(lsb + 1)
-    // console.log(o)
-    const nV = bin2dec(o)
-    // console.log("was ", v.toString(16), "is", nV.toString(16))
-    buf[byte] = nV
-    return buf;
-}
-
-function setDecBits(buf, byte, from, len, value) {
-    const bValue = dec2bin(value, len)
-    // console.log(bValue)
-    for (let i = 0; i < len; i++) {
-        // console.log(i,)
-        buf = setBit(buf, byte, from + i, bValue.charAt(len - 1 - i) == "1")
-    }
-    return buf
-}
-
-function testHexConf() {
-    const dS = new DefaultLoraState();
-    const res = buildHexConfFromState(dS);
-    if (res != defaultHexConf) {
-        throw new Error("hex conf is bugyy got " + res + " insteadof " + defaultHexConf);
-    }
-}
-testHexConf();
---
+++ src/modules/LoraModuleHelpers.ts
@@ -0,0 +1,173 @@
+import * as dbg from '../dbg'
+
+import ConfFileWatcher from '../ConfFileWatcher';
+import * as uConf from '../userConf'
+import * as sys from '../sysUtils'
+import { isPi } from '../platformUtil';
+import * as appPaths from '../filePaths'
+import { exec, execSync } from 'child_process';
+import { LoraState, DefaultLoraState, validateLoraState } from '../types/LoraState';
+import unix from "unix-dgram"
+import fs from 'fs'
+
+export function execOnPiOnly(cmd) {
+    console.log('[lora] cmd will run : ' + cmd)
+    if (isPi) {
+        const res = execSync(cmd).toString();
+        console.log("[lora] cmd resp", res)
+        return res
+    }
+    else
+        execSync('sleep 1');
+}
+
+export function sysctlCmd(opts: string) {
+    return execOnPiOnly(`sudo systemctl ${opts} e32.service`)
+}
+
+
+
+
+
+const clientSock = "/home/pi/e32.rx.data"
+const dataSock = "/run/e32.data"
+
+
+export function createSock(onMessage) {
+    // if (sock) { console.log("[loraSock]  force sock close"); sock.close(); sock = undefined }
+
+    let sock = {} as any;
+    sock = unix.createSocket('unix_dgram', (buf) => {
+        if (!sock.isRegisteredToe32) {
+            console.log('[loraSock] registered ' + buf);
+            sock.isRegisteredToe32 = true;
+            return
+        }
+        if (buf.length == 1 && buf[0] == 0) {
+            // console.log('[loraSock] successfully sent message ');
+            return
+        }
+        onMessage(buf)
+
+
+    });
+    sock.on('error', (e) => {
+        console.error("[loraSock] !!!! sock error", e)
+        sock.isRegisteredToe32 = false
+
+    })
+
+    sock.on('listening', () => {
+        console.log("[loraSock]  sock connected")
+        // register to e32
+        const buf = Buffer.from('')
+        sock.send(buf, 0, buf.length, dataSock);
+    })
+    sock.isRegisteredToe32 = false
+    uConf.setRW(true)
+    if (fs.existsSync(clientSock))
+        fs.unlinkSync(clientSock)
+    sock.bind(clientSock);
+    uConf.setRW(false)
+
+    sock.sendBuf = (buf: Buffer) => {
+        if (!sock.isRegisteredToe32)
+            throw new Error("csock not yet registered")
+        sock.send(buf, 0, buf.length, dataSock);
+    }
+    const oriClose = sock.close
+    sock.close = () => {
+        console.log("[loraSock]  closing sock", sock.isRegisteredToe32)
+        if (sock.isRegisteredToe32)
+            oriClose();
+        else
+            console.error("[loraSock] can't close non listening sock")
+        sock.isRegisteredToe32 = false;
+    }
+    return sock;
+}
+
+/////////////
+// bit helpers
+
+export const defaultHexConf = "C200001B2844"
+
+
+export function buildHexConfFromState(o: LoraState) {
+    //isPi ? execOnPiOnly("e32 -s | grep Settings Raw Value:") :
+    //  "0xc000001b2844"// 
+    if (!validateLoraState(o))
+        throw new Error("invalid state")
+    let b = hexStrToBuf(defaultHexConf);
+    if (b.byteLength != 6) {
+        throw new Error("invalid")
+    }
+    const SPED = 3
+    const CHAN = 4
+    const OPTION = 5
+    b = setDecBits(b, SPED, 0, 3, o.speed)
+    b = setDecBits(b, CHAN, 0, 6, o.channel)
+    b = setBit(b, OPTION, 2, o.fec);
+
+    let res = bufToHexStr(b)
+    return res
+}
+
+function hexStrToBuf(s) {
+    return Buffer.from(s, 'hex')
+
+}
+function bufToHexStr(b: Buffer) {
+    let res = ""
+    for (let i = 0; i < b.length; i++) {
+        let c = b[i].toString(16).toUpperCase()
+        if (c.length < 2) c = "0" + c
+        res += c
+    }
+
+    return res
+}
+function dec2bin(dec, alignMod = 4) {
+    let r = (dec >>> 0).toString(2);
+    while (alignMod > 0 && ((r.length % alignMod) != 0))
+        r = "0" + r
+    return r;
+}
+
+function bin2dec(bin) {
+    return parseInt(bin, 2);
+}
+
+function setBit(buf, byte, bit, value) {
+    const c = !!value ? "1" : "0"
+    const v = buf[byte];
+    // console.log(v)
+    let o = dec2bin(buf[byte]);
+    // console.log(o)
+    const lsb = 7 - bit
+    o = o.substring(0, lsb) + c + o.substring(lsb + 1)
+    // console.log(o)
+    const nV = bin2dec(o)
+    // console.log("was ", v.toString(16), "is", nV.toString(16))
+    buf[byte] = nV
+    return buf;
+}
+
+function setDecBits(buf, byte, from, len, value) {
+    const bValue = dec2bin(value, len)
+    // console.log(bValue)
+    for (let i = 0; i < len; i++) {
+        // console.log(i,)
+        buf = setBit(buf, byte, from + i, bValue.charAt(len - 1 - i) == "1")
+    }
+    return buf
+}
+
+function testHexConf() {
+    const dS = new DefaultLoraState();
+    const res = buildHexConfFromState(dS);
+    if (res != defaultHexConf) {
+        throw new Error("hex conf is bugyy got " + res + " insteadof " + defaultHexConf);
+    }
+}
+testHexConf();
