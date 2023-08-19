dd9d9793206ee4940088fb7ce9932383c03c42c5
tinmar <martin.hermant@gmail.com>
2023-08-19 18:21:06 +0200

    update auto start server to trop on pi only

--- autoStartServer.sh
+++ autoStartServer.sh
@@ -1,19 +1,19 @@
-RCF="/home/pi/.bash_profile"
-if [[ -f $RCF ]]; then
-    echo "init pi"
-    source $RCF
-    SUDO=sudo
-fi
-#cd server
-# npm run run -- --srv
-
 function do_for_sigint() {
     echo "signal interupted"
     sudo systemctl stop e32.service # avoid random locks (maybe)
     exit 1
 }
 
-trap 'do_for_sigint' 2
+RCF="/home/pi/.bash_profile"
+if [[ -f $RCF ]]; then
+    echo "init pi"
+    source $RCF
+    SUDO=sudo
+    trap 'do_for_sigint' 2
+fi
+#cd server
+# npm run run -- --srv
+
 NODE=$(which node) # cause sudo will loose the path
 
 $SUDO $NODE --unhandled-rejections=strict --experimental-modules --es-module-specifier-resolution=node out/index.js --srv -c
